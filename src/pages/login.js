318c55283e59e38219d7d14dc3342a71ce03219d
jdavmo <jdavmo75@gmail.com>
2021-05-16 19:18:07 -0500

    add tailwind css

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,3 +1,25 @@
+import { useContext, useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import FirebaseContext from "../context/firebase";
+
 export default function Login() {
-  return (<p>Login page</p>);
+
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [error, setError] = useState('');
+  const isInvalid = password === '' || emailAddress === '';
+
+  useEffect(() => {
+    document.title = 'Login - Instagram';
+  }, []);
+
+  return (
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      hola mundo
+    </div>
+  );
 };
