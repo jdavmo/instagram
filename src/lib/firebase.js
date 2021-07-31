b9e32669b57ee3c47a3be61586b6c446a9c2915b
Julian David <juliangalvis75@gmail.com>
2021-07-31 00:14:41 -0500

    feat(login-signup): refactor login and add signup

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyBbiZ6Tp_xmwklsdR8lxZ21YgHkFCvmq0k",
-  authDomain: "instagram-davmo.firebaseapp.com",
-  projectId: "instagram-davmo",
-  storageBucket: "instagram-davmo.appspot.com",
-  messagingSenderId: "71440671936",
-  appId: "1:71440671936:web:9a73999118b6758de67925"
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
 
 const firebase = Firebase.initializeApp(config);
